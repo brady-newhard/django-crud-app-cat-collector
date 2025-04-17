ba161c56d6e4e21be25411f8b176dd2fdb96052b
Brady Newhard <brady.russell@gmail.com>
2025-04-16 23:33:31 -0400

    CBV-Feedings

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 # main_app/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
 from .models import Cat
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse
 
@@ -35,6 +37,33 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        # include the cat and feeding_form in the context
+        'cat': cat, 'feeding_form': feeding_form
+    })
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
 
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+    
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # commit=False tells Django to take the data in form.cleaned_data and not save it to the database yet
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
 
