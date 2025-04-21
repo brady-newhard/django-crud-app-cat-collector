fce7a8d7029cdd3c59fb5ddbbba7b48469e69d4f
Brady Newhard <brady.russell@gmail.com>
2025-04-21 00:27:48 -0400

    Models-Auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 from django.urls import reverse
-# A tuple of 2-tuples added above our models
+from datetime import date
+# Import the User
+from django.contrib.auth.models import User
+
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+    
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
-
+    toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
@@ -36,5 +50,6 @@ class Feeding(models.Model):
     def __str__(self):
         # Nice method for obtaining the friendly value of a Field.choice
         return f"{self.get_meal_display()} on {self.date}"
+    
     class Meta:
-        ordering = ['-date']
+        ordering = ['-date']   
